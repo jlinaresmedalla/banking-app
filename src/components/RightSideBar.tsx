f6325a6388d3c8143b75e807daec8f5638241f93
jlinaresmedalla <jlinaresmedalla@gmail.com>
2024-06-23 16:15:13 -0500

    feature: Add categories section on sidebar - jlinaresm

--- src/components/RightSideBar.tsx
+++ src/components/RightSideBar.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { countTransactionCategories } from '@/lib/utils';
 import BankCard from './Bankcard';
+import { Category } from './Category';
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+
   return (
     <aside className="right-sidebar">
       <section className="flex flex-col pb-8">
@@ -49,6 +53,14 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             )}
           </div>
         )}
+        <div className="mt-10 flex flex-1 flex-col gap-6">
+          <h2 className="header-2">Top categories</h2>
+          <div className="space-y-5">
+            {categories.map((category) => (
+              <Category key={category.name} category={category} />
+            ))}
+          </div>
+        </div>
       </section>
     </aside>
   );
